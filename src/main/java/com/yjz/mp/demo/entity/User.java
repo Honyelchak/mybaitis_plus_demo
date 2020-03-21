4d4036c22c6f0c2c3d5e9333d78b5c557015d3fc
Honyelchak <554417388@qq.com>
2020-03-21 11:13:38 +0800

    Initial commit

---
+++ src/main/java/com/yjz/mp/demo/entity/User.java
@@ -0,0 +1,16 @@
+package com.yjz.mp.demo.entity;
+
+import lombok.Data;
+
+import java.util.Date;
+
+/**
+ * @author Honyelchak
+ * @create 2020-03-21 10:35
+ */
+@Data
+public class User {
+
+    private Long id;
+    private String userName;
+}
---
+++ src/main/java/com/yjz/mp/demo/mapper/UserMapper.java
@@ -0,0 +1,11 @@
+package com.yjz.mp.demo.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.yjz.mp.demo.entity.User;
+
+/**
+ * @author Honyelchak
+ * @create 2020-03-21 11:12
+ */
+public interface UserMapper extends BaseMapper<User> {
+}
